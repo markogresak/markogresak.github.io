48d9eca9fd04f7faf728d758e6494eb47487e8ac
Marko Grešak <marko@gresak.io>
2015-05-19 02:07:57 +0200

    replace coffee source with js
    Not worth using coffee for so little code.

--- src/coffee/app.coffee
+++
@@ -1,8 +0,0 @@
-addProjectCount = (count) -> document.getElementById("project-count").innerText = "(#{count})"
-document.addEventListener "DOMContentLoaded", ->
-  addProjectCount 0
-  try
-    request = new XMLHttpRequest()
-    request.open "GET", "https://api.github.com/users/markogresak", true
-    request.onload = -> addProjectCount JSON.parse(request.responseText).public_repos or 0
-    request.send()
---
+++ src/js/app.js
@@ -0,0 +1,17 @@
+(function() {
+  'use strict';
+  var addProjectCount = function (count) {
+    document.getElementById('project-count').innerText = '(' + count + ')';
+  };
+  document.addEventListener('DOMContentLoaded', function () {
+    addProjectCount(0);
+    var request = new XMLHttpRequest();
+    request.open('GET', 'https://api.github.com/users/markogresak', true);
+    request.onload = function () {
+      try {
+        addProjectCount(JSON.parse(request.responseText).public_repos || 0);
+      } catch (e) {}
+    };
+    request.send();
+  });
+})();
