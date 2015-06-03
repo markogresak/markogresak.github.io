45bae2923b16ae3515dc2fc5af9b1925b29f64ba
Marko Grešak <marko@gresak.io>
2015-06-03 20:15:59 +0200

    add file/function to load lib.css
    Based on
    https://developers.google.com/speed/docs/insights/OptimizeCSSDelivery

---
+++ src/js/load-lib-css.js
@@ -0,0 +1,11 @@
+// From Optimize CSS delivery: https://developers.google.com/speed/docs/insights/OptimizeCSSDelivery
+
+var cb = function() {
+  var l = document.createElement('link'); l.rel = 'stylesheet';
+  l.href = 'lib.css';
+  var h = document.getElementsByTagName('head')[0]; h.parentNode.insertBefore(l, h);
+};
+var raf = requestAnimationFrame || mozRequestAnimationFrame ||
+    webkitRequestAnimationFrame || msRequestAnimationFrame;
+if (raf) raf(cb);
+else window.addEventListener('load', cb);
