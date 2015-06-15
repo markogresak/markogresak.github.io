a045ee25d6ab04195e68a0cee9422ce97ed5a2d6
Marko Grešak <marko@gresak.io>
2015-06-15 04:46:57 +0200

    remove no-js text when icons are loaded

--- src/js/load-lib-css.js
+++ src/js/load-lib-css.js
@@ -1,9 +1,11 @@
 // From Optimize CSS delivery: https://developers.google.com/speed/docs/insights/OptimizeCSSDelivery
 
 var cb = function() {
-  var l = document.createElement('link'); l.rel = 'stylesheet';
+  var d = document;
+  for (var el = d.getElementsByClassName('no-js'), i = el.length - 1; i >= 0; i--) el[i].remove();
+  var l = d.createElement('link'); l.rel = 'stylesheet';
   l.href = 'css/lib.css';
-  var h = document.getElementsByTagName('head')[0]; h.parentNode.insertBefore(l, h);
+  var h = d.getElementsByTagName('head')[0]; h.parentNode.insertBefore(l, h);
 };
 var raf = requestAnimationFrame || mozRequestAnimationFrame ||
     webkitRequestAnimationFrame || msRequestAnimationFrame;
