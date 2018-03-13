3e753a67613d0df84a65ffe1b1218cb0f5d77cdf
Marko Grešak <gresak.marko@gmail.com>
2018-03-13 19:46:39 +0100

    Remove custom icons, use font-awesome 5

--- src/js/app.js
+++ src/js/app.js
@@ -1,19 +1,30 @@
 (function() {
   'use strict';
-  var addProjectCount = function (count) {
-    if (count > 0) {
-      document.getElementById('c').innerText = count;
-      document.getElementById('cw').style.display = 'inline';
-    }
-  };
-  document.addEventListener('DOMContentLoaded', function () {
+  var d = document;
+  var cb = function() {
+    var el, i, l, h;
+    for (el = d.getElementsByClassName('no-js'), i = el.length - 1; i >= 0; i--) el[i].remove();
+    (['https://use.fontawesome.com/releases/v5.0.8/css/all.css', 'css/lib.css']).forEach(function (s) {
+      l = d.createElement('link'); l.rel = 'stylesheet';
+      l.href = s;
+      h = d.getElementsByTagName('head')[0]; h.parentNode.insertBefore(l, h);
+    });
+
     var request = new XMLHttpRequest();
     request.open('GET', 'https://api.github.com/users/markogresak', true);
     request.onload = function () {
       try {
-        addProjectCount(JSON.parse(request.responseText).public_repos || 0);
+        var count = JSON.parse(request.responseText).public_repos || 0;
+        if (count > 0) {
+          d.getElementById('c').innerText = count;
+          d.getElementById('cw').style.display = 'inline';
+        }
       } catch (e) {}
     };
     request.send();
-  });
+  };
+  var raf = requestAnimationFrame || mozRequestAnimationFrame ||
+      webkitRequestAnimationFrame || msRequestAnimationFrame;
+  if (raf) raf(cb);
+  else window.addEventListener('load', cb);
 })();
