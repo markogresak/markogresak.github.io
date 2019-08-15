7d26015691dbed69c81853f468071678e76d2fea
Marko Grešak <marko@gresak.io>
2019-08-15 16:42:56 +0200

    Replace icon

--- gatsby-config.js
+++ gatsby-config.js
@@ -76,7 +76,7 @@ module.exports = {
         background_color: `#ffffff`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `content/assets/favicon.ico`,
+        icon: `content/assets/favicon.png`,
       },
     },
     `gatsby-plugin-offline`,
