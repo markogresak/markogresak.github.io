11b86aa4d17d22a95cd95db12afeadad29585547
Marko Grešak <marko@gresak.io>
2019-08-16 20:54:35 +0200

    Render links inside AboutMe

--- src/components/about-me.js
+++ src/components/about-me.js
@@ -2,31 +2,53 @@ import React from "react"
 import { useStaticQuery, graphql } from "gatsby"
 import Image from "gatsby-image"
 import styled from "@emotion/styled"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 
+import Layout from "./layout"
 import { rhythm, scale } from "../utils/typography"
 
 const Wrapper = styled.div`
-  text-align: center;
   font-weight: 300;
-  padding: 0 ${rhythm(0.5)};
+  display: flex;
+
+  @media (max-width: 560px) {
+    flex-wrap: wrap;
+    justify-content: center;
+  }
+`
+
+const ImageWrapper = styled.div`
+  flex: 0 0 150px;
+  margin-right: ${rhythm(1)};
+  margin-top: ${rhythm(0.5)};
+
+  @media (max-width: 560px) {
+    margin: 0 0 ${rhythm(1)};
+  }
 `
 
 const Name = styled.h1`
-  ${scale(1.6)};
-  margin-top: ${rhythm(1)};
+  ${scale(1)};
+  margin: 0;
   border-bottom: none;
 `
 
 const AboutMeList = styled.ul`
   list-style: none;
-  ${scale(0.2)};
+  ${scale(0.1)};
   margin: 0;
-  margin-top: ${rhythm(1.5)};
 `
 
-const AboutMeItem = styled.li`
+const AboutMeItem = styled.li``
+
+const LinksList = styled.ul`
+  list-style: none;
+  margin: 0;
   margin-top: ${rhythm(0.5)};
-  margin-bottom: 0;
+`
+
+const IconWrapper = styled.span`
+  margin-right: ${rhythm(0.3)};
 `
 
 const AboutMe = () => {
@@ -34,7 +56,7 @@ const AboutMe = () => {
     query AboutMeQuery {
       avatar: file(absolutePath: { regex: "/profile-pic.jpg/" }) {
         childImageSharp {
-          fixed(width: 300, height: 300, quality: 100) {
+          fixed(width: 150, height: 150, quality: 100) {
             ...GatsbyImageSharpFixed
           }
         }
@@ -43,33 +65,65 @@ const AboutMe = () => {
         siteMetadata {
           title
           fullDescription
+          links {
+            title
+            items {
+              title
+              href
+              icon
+              color
+            }
+          }
+        }
+      }
+      github {
+        user(login: "markogresak") {
+          repositories(privacy: PUBLIC) {
+            totalCount
+          }
         }
       }
     }
   `)
 
-  const { title, fullDescription } = data.site.siteMetadata
+  const { title, fullDescription, links } = data.site.siteMetadata
+  const { totalCount: repositoriesCount } = data.github.user.repositories
 
   return (
-    <Wrapper>
-      <Name>{title}</Name>
-      <Image
-        fixed={data.avatar.childImageSharp.fixed}
-        alt="Profile image"
-        style={{
-          display: "block",
-          margin: "0 auto",
-        }}
-        imgStyle={{
-          borderRadius: `50%`,
-        }}
-      />
-      <AboutMeList>
-        {fullDescription.map((item, i) => (
-          <AboutMeItem key={i} dangerouslySetInnerHTML={{ __html: item }} />
-        ))}
-      </AboutMeList>
-    </Wrapper>
+    <Layout>
+      <Wrapper>
+        <ImageWrapper>
+          <Image
+            fixed={data.avatar.childImageSharp.fixed}
+            alt="Profile image"
+            imgStyle={{
+              borderRadius: `50%`,
+            }}
+          />
+        </ImageWrapper>
+        <div>
+          <Name>{title}</Name>
+          <AboutMeList>
+            {fullDescription.map((item, i) => (
+              <AboutMeItem key={i} dangerouslySetInnerHTML={{ __html: item }} />
+            ))}
+          </AboutMeList>
+          <LinksList>
+            {links.items.map(({ title, href, icon, color }) => (
+              <li key={title}>
+                <IconWrapper>
+                  <FontAwesomeIcon icon={["fab", icon]} color={color} />
+                </IconWrapper>
+                <a href={href} title={title}>
+                  {title}
+                  {title === "Github" && <> ({repositoriesCount} projects)</>}
+                </a>
+              </li>
+            ))}
+          </LinksList>
+        </div>
+      </Wrapper>
+    </Layout>
   )
 }
 
