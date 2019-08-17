d9ac3b8c16b12da5e5d4ac43699613f76c545be6
Marko Grešak <marko@gresak.io>
2019-08-17 13:21:23 +0200

    Update logo colors, remove logo image

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,44 +1,23 @@
 import React from "react"
-import { useStaticQuery, graphql } from "gatsby"
-import Image from "gatsby-image"
 import styled from "@emotion/styled"
 
-import { rhythm } from "../utils/typography"
+import { primaryColor, textColor } from "../utils/colors"
 
 const Container = styled.span`
   display: inline-flex;
   align-items: center;
 `
 
-const LogoImage = styled.span`
-  margin-right: ${rhythm(0.5)};
-`
-
 const TitleMain = styled.span`
-  color: #2d99ba;
+  color: ${primaryColor};
 `
 const TitleSecondary = styled.span`
-  color: #83a0a5;
+  color: ${textColor};
 `
 
 const Logo = () => {
-  const data = useStaticQuery(graphql`
-    query LogoQuery {
-      logo: file(absolutePath: { regex: "/logo.png/" }) {
-        childImageSharp {
-          fixed(width: 67, height: 58, quality: 100) {
-            ...GatsbyImageSharpFixed
-          }
-        }
-      }
-    }
-  `)
-
   return (
     <Container>
-      <LogoImage>
-        <Image fixed={data.logo.childImageSharp.fixed} alt="Profile image" />
-      </LogoImage>
       <TitleMain>Out</TitleMain>
       <TitleSecondary>Of</TitleSecondary>
       <TitleMain>Memory</TitleMain>
