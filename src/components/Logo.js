a99948e085f9efc3ef4753160360f13affc9d943
Marko Grešak <marko@gresak.io>
2019-08-19 19:18:44 +0200

    Increase size of logo

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,7 +1,14 @@
 import React from "react"
 import styled from "@emotion/styled"
 
+import { scale } from "../utils/typography"
 import { primaryColor, textColor } from "../utils/colors"
+
+const Title = styled.span`
+  ${scale(1)};
+  line-height: 1;
+  font-weight: 600;
+`
 
 const TitleMain = styled.span`
   color: ${primaryColor};
@@ -12,11 +19,11 @@ const TitleSecondary = styled.span`
 
 const Logo = () => {
   return (
-    <>
+    <Title>
       <TitleMain>Out</TitleMain>
       <TitleSecondary>Of</TitleSecondary>
       <TitleMain>Memory</TitleMain>
-    </>
+    </Title>
   )
 }
 
