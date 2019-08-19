e1dbe1daf9a28e3674dbe0547bdb25f3d4088630
Marko Grešak <marko@gresak.io>
2019-08-19 21:51:11 +0200

    Use default link style for ReadMore

--- src/components/ReadMore.js
+++ src/components/ReadMore.js
@@ -5,20 +5,11 @@ import { css } from "@emotion/core"
 import AnimatedArrow from "./AnimatedArrow"
 
 import { rhythm, scale } from "../utils/typography"
-import { primaryLightColor, primaryColor } from "../utils/colors"
 
 const readMoreStyle = css`
-  display: block;
   margin-top: ${rhythm(1)};
   text-decoration: none !important;
-  font-weight: 500;
   ${scale(0.1)}
-  transition: color 0.2s;
-  color: ${primaryLightColor};
-
-  &:hover {
-    color: ${primaryColor};
-  }
 `
 
 const ReadMore = ({ children }) => {
