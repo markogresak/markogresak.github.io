f845e537627cbe57056dba498e01841e298268b8
Marko Grešak <marko@gresak.io>
2019-08-20 17:08:02 +0200

    Increase max-width of page section

--- src/components/page.js
+++ src/components/page.js
@@ -43,7 +43,7 @@ const globalStyles = css`
 const Layout = styled.div`
   margin-left: auto;
   margin-right: auto;
-  max-width: ${rhythm(24)};
+  max-width: ${rhythm(27)};
   padding: ${rhythm(1.5)} ${rhythm(3 / 4)};
 `
 
