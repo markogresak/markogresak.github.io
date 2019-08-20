1bd1b2076b7a80a901823e724d5c34e2925c8560
Marko Grešak <marko@gresak.io>
2019-08-20 18:39:51 +0200

    Increase image max-width

--- gatsby-config.js
+++ gatsby-config.js
@@ -73,7 +73,7 @@ module.exports = {
           {
             resolve: `gatsby-remark-images`,
             options: {
-              maxWidth: 590,
+              maxWidth: 663,
             },
           },
           {
