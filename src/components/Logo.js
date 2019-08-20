f666e90bbc0dac54b262d1acabb3d96d42569607
Marko Grešak <marko@gresak.io>
2019-08-20 12:11:13 +0200

    Animate logo letters

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,28 +1,79 @@
 import React from "react"
 import styled from "@emotion/styled"
 
-import { scale } from "../utils/typography"
 import { primaryColor, textColor } from "../utils/colors"
 
-const Title = styled.span`
-  ${scale(1)};
+const FONT_SIZE = 32
+const FONT_SIZE_SMALL = 12
+const LETTER_APPEAR_DELAY = 0.1
+
+const letters = [
+  { letter: "O", color: primaryColor, visible: true },
+  { letter: "u", color: primaryColor, visible: false },
+  { letter: "t", color: primaryColor, visible: false },
+  { letter: "O", color: textColor, visible: true },
+  { letter: "f", color: textColor, visible: false },
+  { letter: "M", color: primaryColor, visible: false },
+  { letter: "e", color: primaryColor, visible: false },
+  { letter: "m", color: primaryColor, visible: false },
+  { letter: "o", color: primaryColor, visible: false },
+  { letter: "r", color: primaryColor, visible: false },
+  { letter: "y", color: primaryColor, visible: false },
+].map((letter, i) => ({
+  ...letter,
+  index: i + 1,
+}))
+
+const getAnimatedLetters = () => letters.filter(({ visible }) => !visible)
+
+const Container = styled.span`
   font-weight: 600;
 `
 
-const TitleMain = styled.span`
-  color: ${primaryColor};
+const Letter = styled.span`
+  color: ${({ color }) => color};
+  font-size: ${({ visible }) => (visible ? FONT_SIZE : FONT_SIZE_SMALL)}px;
+  transition: font-size ${round(LETTER_APPEAR_DELAY * 3)}s;
+
+  ${getAnimatedLetters().map(
+    ({ index }, i) => `
+      &:nth-of-type(${letters.length - index}) {
+        transition-delay: ${getDelay(i)}s;
+      }
+    `
+  )};
+
+  ${Container}:hover & {
+    font-size: ${FONT_SIZE}px;
+
+    ${getAnimatedLetters().map(
+      ({ index }, i) => `
+        &:nth-of-type(${index}) {
+          transition-delay: ${getDelay(i)}s;
+        }
+      `
+    )};
+  }
 `
-const TitleSecondary = styled.span`
-  color: ${textColor};
-`
+
+function getDelay(i) {
+  return i === 0 ? 0 : round(Math.log(i ** 3) * LETTER_APPEAR_DELAY)
+}
+
+function round(n) {
+  // using `round(x*1e4)/1e4` to round to 4 decimal places
+  return Math.round(n * 1e4) / 1e4
+}
 
 const Logo = () => {
   return (
-    <Title>
-      <TitleMain>Out</TitleMain>
-      <TitleSecondary>Of</TitleSecondary>
-      <TitleMain>Memory</TitleMain>
-    </Title>
+    <Container>
+      {letters.map(({ letter, color, visible, index }) => (
+        <Letter key={index} color={color} visible={visible}>
+          {letter}
+        </Letter>
+      ))}
+    </Container>
   )
 }
 
