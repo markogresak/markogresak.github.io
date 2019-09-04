23e5d6720fd85de2d1a81210754c52b7ae99fd7a
Marko Grešak <marko@gresak.io>
2019-09-04 18:11:30 +0200

    Accept optional links in BlogPage footer

--- src/components/BlogPage.js
+++ src/components/BlogPage.js
@@ -1,15 +1,17 @@
 import React from 'react'
-import { Link } from 'gatsby'
 import styled from '@emotion/styled'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 import Page from './Page'
 import AboutMe from './AboutMe'
 import Logo from './Logo'
+import Link from './Link'
+import LinksList from './LinksList'
 
 import { rhythm } from '../utils/typography'
 import { primaryColor } from '../utils/colors'
 
-const Header = styled.header`
+const HeaderContainer = styled.header`
   margin-bottom: ${rhythm(2)};
 `
 
@@ -18,26 +20,22 @@ const HeaderLogo = styled.h2`
   border-bottom: none;
 `
 
-const Footer = styled.footer`
+const FooterContainer = styled.footer`
+  margin-top: ${rhythm(2)};
+`
+
+const FooterMain = styled.div`
   border-top: 2px solid ${primaryColor};
-  margin-top: ${rhythm(1.5)};
+  margin-top: ${rhythm(0.5)};
   padding-top: ${rhythm(1.5)};
 `
 
-const BlogPage = ({ title, description, children }) => {
-  return (
-    <Page title={title} description={description}>
-      {children}
-    </Page>
-  )
-}
-
-BlogPage.Header = ({ children, withLink }) => (
-  <Header>
+const Header = ({ children, withLink }) => (
+  <HeaderContainer>
     <HeaderLogo>
       {withLink ? (
         <Link
-          to="/blog"
+          href="/blog"
           title="OutOfMemory"
           css={{ boxShadow: 'none !important' }}
         >
@@ -48,14 +46,49 @@ BlogPage.Header = ({ children, withLink }) => (
       )}
     </HeaderLogo>
     {children}
-  </Header>
+  </HeaderContainer>
 )
 
-BlogPage.Footer = ({ children }) => (
-  <Footer>
-    <AboutMe linkToHome />
-    {children}
-  </Footer>
-)
+const Footer = ({ children, links = [] }) => {
+  return (
+    <FooterContainer>
+      <LinksList
+        links={[
+          ...links,
+        ]}
+      >
+        {({ title, icon, iconColor }) => (
+          <>
+            {icon && (
+              <FontAwesomeIcon
+                icon={icon.split(' ')}
+                color={iconColor}
+                css={{ marginRight: rhythm(0.25) }}
+                // force same width on all icons
+                style={{ width: '1em' }}
+              />
+            )}
+            {title}
+          </>
+        )}
+      </LinksList>
+      <FooterMain>
+        <AboutMe linkToHome />
+        {children}
+      </FooterMain>
+    </FooterContainer>
+  )
+}
+
+const BlogPage = ({ title, description, children }) => {
+  return (
+    <Page title={title} description={description}>
+      {children}
+    </Page>
+  )
+}
+
+BlogPage.Header = Header
+BlogPage.Footer = Footer
 
 export default BlogPage
