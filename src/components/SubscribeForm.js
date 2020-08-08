4e3a50be32c94c1bbf936bd1a43fe4af4712cdc9
Marko Grešak <marko@gresak.io>
2020-08-08 19:28:22 +0200

    Update subscribe form to make it less intrusive

--- src/components/SubscribeForm.js
+++ src/components/SubscribeForm.js
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from 'react'
 import styled from '@emotion/styled'
+import useIsInViewport from 'use-is-in-viewport'
 
 import { rhythm } from '../utils/typography'
-import { primaryColor, bgColor } from '../utils/colors'
+import { primaryColor, bgColor, textColor } from '../utils/colors'
 
 const FromContainer = styled.div`
   margin-top: ${rhythm(1)};
@@ -11,22 +12,51 @@ const FromContainer = styled.div`
     box-shadow: none !important;
   }
 
+  .formkit-input,
   .formkit-submit {
-    background: ${primaryColor} !important;
-    color: ${bgColor} !important;
+    background: ${bgColor} !important;
+    color: ${textColor} !important;
+    border: 1px solid ${textColor} !important;
+    line-height: 1;
+    transition: color, border-color 0.2s;
+  }
+
+  .formkit-input {
+    &:focus {
+      border-color: ${primaryColor} !important;
+    }
+  }
+
+  .formkit-submit {
+    &:hover,
+    &:focus {
+      color: ${primaryColor} !important;
+      border-color: ${primaryColor} !important;
+    }
+
+    span {
+      background: ${bgColor} !important;
+    }
   }
 `
 
 const SubscribeForm = () => {
+  const [isInViewport, containerRef] = useIsInViewport()
   const [html, setHtml] = useState('')
 
   useEffect(() => {
-    import('../utils/subscribeFormMarkup').then((module) => {
-      setHtml(module.default)
-    })
-  })
+    if (isInViewport && !html) {
+      import('../utils/subscribeFormMarkup').then((module) => {
+        setHtml(module.default)
+      })
+    }
+  }, [isInViewport, html])
 
-  return html && <FromContainer dangerouslySetInnerHTML={{ __html: html }} />
+  return (
+    <div ref={containerRef}>
+      {html && <FromContainer dangerouslySetInnerHTML={{ __html: html }} />}
+    </div>
+  )
 }
 
 export default SubscribeForm
