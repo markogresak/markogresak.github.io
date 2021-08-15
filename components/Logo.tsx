f56cfb22301f4d9dceff20268db2c8e3bb4581d2
Marko Grešak <marko@gresak.io>
2021-08-15 22:34:46 +0200

    Use display: inline for logo to fix the tap target on mobile

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,6 +1,6 @@
 export const Logo = () => {
   return (
-    <h2 className="text-3xl leading-none font-semibold">
+    <h2 className="text-3xl leading-none font-semibold inline">
       <span className="text-blue-700 dark:text-purple-400">Out</span>
       <span className="text-gray-500">Of</span>
       <span className="text-blue-700 dark:text-purple-400">Memory</span>
