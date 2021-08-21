f53847e10bd01942f17d769e8c5a9a777bd4d2eb
Marko Grešak <marko@gresak.io>
2021-08-21 23:52:52 +0200

    Use npx with esbuild command
    In case esbuild is not installed

--- build-nightwindInitSource.sh
+++ build-nightwindInitSource.sh
@@ -1,3 +1,3 @@
 outfile="./lib/nightwindInitCode.ts"
-echo "// Output of \`esbuild --minify ./lib/nightwindInitSource.ts\`" > $outfile
-echo "export const nightwindInitCode = \`$(esbuild --minify ./lib/nightwindInitSource.ts)\`" >> $outfile
+echo "// Output of \`npx esbuild --minify ./lib/nightwindInitSource.ts\`" > $outfile
+echo "export const nightwindInitCode = \`$(npx esbuild --minify ./lib/nightwindInitSource.ts)\`" >> $outfile
